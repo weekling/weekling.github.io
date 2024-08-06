056e6b6156949f06869f353d99b8fe970a3e90fe
Alex Suddendorf <zatttel@gmail.com>
2024-08-06 19:36:58 +0200

    Initial Commit

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+importScripts('./ngsw-worker.js');
+
+self.addEventListener('push', event => {
+  console.log("Push event received")
+  const data = event.data.json();
+  self.registration.showNotification(data.title, {
+    icon: 'assets/icons/android/logo-notif.png',
+    badge: 'assets/icons/android/badge.png',
+    body: data.content,
+    data: data,
+    tag: data.tag,
+    vibrate: [200]
+  });
+});
+
+// self.addEventListener('notificationclick', event => {
+//   console.log('Notification click event');
+//
+//   event.notification.close(); // Close the notification
+//
+//   event.waitUntil(
+//     clients.matchAll({ type: 'window', includeUncontrolled: true }).then(clientList => {
+//       for (const client of clientList) {
+//         // If the PWA is already open, focus it
+//         if (client.url === event.notification.data.url && 'focus' in client) {
+//           return client.focus();
+//         }
+//       }
+//       // If the PWA is not open, open a new window
+//       if (clients.openWindow) {
+//         return clients.openWindow(event.notification.data.url);
+//       }
+//     })
+//   );
+// });
+
